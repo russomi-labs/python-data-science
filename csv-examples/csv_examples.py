759ec421810895c59345c9322f3bee1818e742b5
Michael Russo <russo.mail@gmail.com>
2022-05-16 12:51:03 -0400

    Adds csv examples
    https://realpython.com/python-csv/

---
+++ csv-examples/csv_examples.py
@@ -0,0 +1,62 @@
+import csv
+import pandas
+
+# csv.reader
+with open("employee_birthday.txt") as csv_file:
+    csv_reader = csv.reader(csv_file, delimiter=",")
+    line_count = 0
+    for row in csv_reader:
+        if line_count == 0:
+            print(f'Column names are {", ".join(row)}')
+            line_count += 1
+        else:
+            print(
+                f"\t{row[0]} works in the {row[1]} department, and was born in {row[2]}."
+            )
+            line_count += 1
+    print(f"Processed {line_count} lines.")
+
+# csv.DictReader
+with open("employee_birthday.txt", mode="r") as csv_file:
+    csv_reader = csv.DictReader(csv_file)
+    line_count = 0
+    for row in csv_reader:
+        if line_count == 0:
+            print(f'Column names are {", ".join(row)}')
+            line_count += 1
+        print(
+            f'\t{row["name"]} works in the {row["department"]} department, and was born in {row["birthday month"]}.'
+        )
+        line_count += 1
+    print(f"Processed {line_count} lines.")
+
+# csv.writer
+with open("employee_file.csv", mode="w") as employee_file:
+    employee_writer = csv.writer(
+        employee_file, delimiter=",", quotechar='"', quoting=csv.QUOTE_MINIMAL
+    )
+
+    employee_writer.writerow(["John Smith", "Accounting", "November"])
+    employee_writer.writerow(["Erica Meyers", "IT", "March"])
+
+# csv.DictWriter
+with open("employee_file2.csv", mode="w") as csv_file:
+    fieldnames = ["emp_name", "dept", "birth_month"]
+    writer = csv.DictWriter(csv_file, fieldnames=fieldnames)
+
+    writer.writeheader()
+    writer.writerow(
+        {"emp_name": "John Smith", "dept": "Accounting", "birth_month": "November"}
+    )
+    writer.writerow({"emp_name": "Erica Meyers", "dept": "IT", "birth_month": "March"})
+
+# pandas
+df = pandas.read_csv(
+    "hrdata.csv",
+    index_col="Employee",
+    parse_dates=["Hired"],
+    header=0,
+    names=["Employee", "Hired", "Salary", "Sick Days"],
+)
+df.to_csv("hrdata_modified.csv")
+print(df)
